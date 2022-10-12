9ebd9dc0f1573b5a32ffedcf4fb152ba53c6a867
palledavinay <101565026+palledavinay@users.noreply.github.com>
2022-10-12 14:27:30 +0530

    firts program

---
+++ sample.java
@@ -0,0 +1,10 @@
+package in.ineuron.main;
+
+public class IneuronFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ sample1.java
@@ -0,0 +1,12 @@
+package in.ineuron.main;
+
+public class FisrtProgramvinay {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
