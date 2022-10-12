5d7d64f65f2d4a72ed328f65d12e5154896738d3
palledavinay <101565026+palledavinay@users.noreply.github.com>
2022-10-12 15:09:11 +0530

    changes made in sample1 .java

--- sample1.java
+++ sample1.java
@@ -3,8 +3,8 @@ package in.ineuron.main;
 public class FisrtProgramvinay {
 
 	public static void main(String[] args) {
-		int a=10,b=20;
-		int c=a+c;
+		int a=10,d=20;
+		int c=a+d;
 		System.out.println(c);
 
 	}
